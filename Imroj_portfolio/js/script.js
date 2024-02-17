106e9825ca7e644f31fced465c8ef8cfa487ddcd
Imroj 2020831050 <107574392+Imroj-Hassan@users.noreply.github.com>
2024-02-17 21:13:23 +0600

    Add files via upload

---
+++ Imroj_portfolio/js/script.js
@@ -0,0 +1,8 @@
+/*======================= typing animation ====================== */
+
+var typed = new Typed(".typing" ,{
+    strings : ["","Fast Lerner","Passionate Worker","Dedicated Person","Research Assistant"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
